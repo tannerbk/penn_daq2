1c5adfab865caac78a2e4876a5a472e386b64dc2
Tanner Kaptanoglu <tannerk@sas.upenn.edu>
2018-06-10 16:15:36 -0400

    add update test status function

--- src/db/DetectorDB.h
+++ src/db/DetectorDB.h
@@ -10,14 +10,23 @@ void CloseDetectorDBConnection(PGconn* detectorDB);
 int CheckResultsStatus(PGresult* qResult, PGconn* detectorDB);
 
 // These are called ultimately from Create FEC Doc command
+// Load zdisc values to the detector database. These are measured values and are not
+// a part of the Fec Doc, which only stores hardware settings
 int LoadZDiscToDetectorDB(JsonNode* doc, int crate, int slot, const char* ecalID, PGconn* detectorDB);
 
+// Load measured GT valid lengths to detector database
 int LoadGTValidsToDetectorDB(JsonNode* doc, int crate, int slot, const char* ecalID, PGconn* detectorDB);
 
+// Load channel problems to the detector database
+int LoadChannelStatusToDetectorDB(JsonNode* doc, int crate, int slot, const char* ecalID, PGConn* detectorDB);
+
+// Load FEC Documents to the detector database
 int LoadFECDocToDetectorDB(JsonNode* doc, int crate, int slot, const char* ecalID, PGconn* detectorDB);
 
+// Update channel status in detector database for bad 
 int LoadBadDiscToDetectorDB(int crate, int slot, int channel, PGconn* detectorDB);
 
+// Update 
 int UpdateChannelStatus(int crate, int slot, int channel, PGconn* detectorDB);
 
 // Tool used for formatting SQL query for arrays size
