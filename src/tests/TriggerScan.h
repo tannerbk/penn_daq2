74730f719227ee78847bdd41aac98bfb0be39123
Eric Marzec <marzece@sas.upenn.edu>
2018-08-28 17:04:25 -0400

    Updates to trigger scan

--- src/tests/TriggerScan.h
+++ src/tests/TriggerScan.h
@@ -3,7 +3,9 @@
 
 #include <unistd.h>
 
-int TriggerScan(uint32_t crateMask, uint32_t *slotMasks, int triggerSelect, int dacSelect, int maxNhit, int minThresh, const char* fileName, int quickMode);
+int TriggerScan(const uint32_t crateMask, uint32_t *slotMasks, const int
+        triggerSelect, const int dacSelect, int maxNhit, const int
+        minThresh, const char* fileName, const int quickMode, const int tub);
 
 #endif
 
