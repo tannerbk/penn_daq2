55ea209a978a876cb52c2c5f109e799448765305
tannerbk <tannerk@sas.upenn.edu>
2019-08-10 12:32:13 -0400

    add test mask check for gtvalid

--- src/tests/ECAL.cpp
+++ src/tests/ECAL.cpp
@@ -287,9 +287,10 @@ int ECAL(uint32_t crateMask, uint32_t *slotMasks, uint32_t testMask, int quickFl
     // pass quick flag as setOnly if you want to speed this up
     // commented out for now so gtvalid measurement is forced
     // runs on one crate at a time
-    for (int i=0;i<MAX_XL3_CON;i++)
-      if ((0x1<<i) & crateMask)
-        GTValidTest((0x1<<i),slotMasks,0xFFFFFFFF,410,0,0,1,1,0,1);
+    if ((0x1<<testCounter) & testMask)
+      for (int i=0;i<MAX_XL3_CON;i++)
+        if ((0x1<<i) & crateMask)
+          GTValidTest((0x1<<i),slotMasks,0xFFFFFFFF,410,0,0,1,1,0,1);
     testCounter++;
 
     MTCInit(1);
